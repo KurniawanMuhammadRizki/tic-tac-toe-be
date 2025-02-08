3f4c6789392953b67ae05c624838ccd413a5efe4
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2025-02-08 15:35:34 +0700

    feat(game) refactor: change data type to entity in Game Entity

--- src/main/java/com/kuki/tic_tac_toe_be/game/entity/Game.java
+++ src/main/java/com/kuki/tic_tac_toe_be/game/entity/Game.java
@@ -1,13 +1,16 @@
 package com.kuki.tic_tac_toe_be.game.entity;
 
+import com.kuki.tic_tac_toe_be.board.entity.Board;
+import com.kuki.tic_tac_toe_be.player.entity.Player;
 import lombok.Data;
 
 @Data
 public class Game {
-     private String gameId;
-     private String [][] board;
-     private String currentPlayer;
-     private String status;
-     private String winner;
+     private String id;
+     private Board board;
+     private Player currentPlayer;
+     private Player winner;
+     private GameStatus status;
+
 }
 
---
+++ src/main/java/com/kuki/tic_tac_toe_be/game/entity/GameStatus.java
@@ -0,0 +1,5 @@
+package com.kuki.tic_tac_toe_be.game.entity;
+
+public enum GameStatus {
+          PLAYING, WIN, DRAW
+}
