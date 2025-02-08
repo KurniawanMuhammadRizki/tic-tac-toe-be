90b3727f81df6a77c3197d6eb45e3f02501fa2a7
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2025-02-08 14:24:25 +0700

    initial commit

---
+++ src/main/java/com/kuki/tic_tac_toe_be/TicTacToeBeApplication.java
@@ -0,0 +1,13 @@
+package com.kuki.tic_tac_toe_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuki/tic_tac_toe_be/TicTacToeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuki.tic_tac_toe_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
