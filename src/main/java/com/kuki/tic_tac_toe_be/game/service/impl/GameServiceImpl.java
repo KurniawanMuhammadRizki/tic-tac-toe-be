8793cfd8e54545664456e3a24ba0ca2072cb9138
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2025-02-08 17:15:26 +0700

    feat(game) add: game repository to game service

--- src/main/java/com/kuki/tic_tac_toe_be/game/service/impl/GameServiceImpl.java
+++ src/main/java/com/kuki/tic_tac_toe_be/game/service/impl/GameServiceImpl.java
@@ -4,6 +4,7 @@ import com.kuki.tic_tac_toe_be.board.entity.Board;
 import com.kuki.tic_tac_toe_be.board.service.BoardService;
 import com.kuki.tic_tac_toe_be.game.entity.Game;
 import com.kuki.tic_tac_toe_be.game.entity.GameStatus;
+import com.kuki.tic_tac_toe_be.game.repository.GameRepository;
 import com.kuki.tic_tac_toe_be.game.service.GameService;
 import com.kuki.tic_tac_toe_be.player.entity.Player;
 import com.kuki.tic_tac_toe_be.player.service.PlayerService;
@@ -14,9 +15,13 @@ import org.springframework.stereotype.Service;
 public class GameServiceImpl implements GameService {
      private final BoardService boardService;
      private final PlayerService playerService;
-     public GameServiceImpl(BoardService boardService, PlayerService playerService) {
+     private final GameRepository gameRepository;
+
+
+     public GameServiceImpl(BoardService boardService, PlayerService playerService, GameRepository gameRepository) {
           this.boardService = boardService;
           this.playerService = playerService;
+          this.gameRepository = gameRepository;
      }
 
      @Override
@@ -30,6 +35,8 @@ public class GameServiceImpl implements GameService {
           game.setBoard(board);
           game.setCurrentPlayer(p1);
           game.setStatus(GameStatus.PLAYING);
+          gameRepository.save(game);
+
           return game;
      }
 
