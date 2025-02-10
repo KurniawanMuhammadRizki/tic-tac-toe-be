3914c064a8d1341f2b92a74ed9f417c84dd86cf5
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2025-02-10 10:42:19 +0700

    feat(board) fix: win condition bug when board > 3

--- src/main/java/com/kuki/tic_tac_toe_be/board/service/impl/BoardServiceImpl.java
+++ src/main/java/com/kuki/tic_tac_toe_be/board/service/impl/BoardServiceImpl.java
@@ -7,31 +7,38 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BoardServiceImpl implements BoardService {
+
      @Override
-     public  Board createBoard(int size){
-          if(size<3){
+     public Board createBoard(int size) {
+          if (size < 3) {
                throw new GameException("Board size must be greater than 3");
           }
           return new Board(size);
      }
+
      @Override
-     public boolean isValidMove(Board board, int row, int col){
+     public boolean isValidMove(Board board, int row, int col) {
           return row >= 0 && row < board.getSize() &&
                   col >= 0 && col < board.getSize() &&
                   board.getGrid()[row][col].isEmpty();
      }
 
      @Override
-     public void updateBoard(Board board, int row, int col, String symbol){
+     public void updateBoard(Board board, int row, int col, String symbol) {
           if (!isValidMove(board, row, col)) {
                throw new GameException("Invalid move");
           }
           board.getGrid()[row][col] = symbol;
      }
 
+     private int getWinCondition(Board board) {
+          return (board.getSize() > 3) ? 4 : 3;
+     }
+
      private boolean checkRows(Board board, String symbol) {
+          int winCondition = getWinCondition(board);
           for (int i = 0; i < board.getSize(); i++) {
-               if (checkLine(board.getGrid()[i], symbol)) {
+               if (checkLine(board.getGrid()[i], symbol, winCondition)) {
                     return true;
                }
           }
@@ -39,51 +46,76 @@ public class BoardServiceImpl implements BoardService {
      }
 
      private boolean checkColumns(Board board, String symbol) {
+          int winCondition = getWinCondition(board);
           for (int col = 0; col < board.getSize(); col++) {
-               boolean win = true;
+               int count = 0;
                for (int row = 0; row < board.getSize(); row++) {
-                    if (!board.getGrid()[row][col].equals(symbol)) {
-                         win = false;
-                         break;
+                    if (board.getGrid()[row][col].equals(symbol)) {
+                         count++;
+                         if (count == winCondition) return true;
+                    } else {
+                         count = 0;
                     }
                }
-               if (win) return true;
           }
           return false;
      }
 
      private boolean checkDiagonals(Board board, String symbol) {
-          boolean mainDiag = true;
-          boolean antiDiag = true;
+          int winCondition = getWinCondition(board);
+          int size = board.getSize();
 
-          for (int i = 0; i < board.getSize(); i++) {
-               if (!board.getGrid()[i][i].equals(symbol)) {
-                    mainDiag = false;
-               }
-               if (!board.getGrid()[i][board.getSize()-1-i].equals(symbol)) {
-                    antiDiag = false;
+          for (int startRow = 0; startRow <= size - winCondition; startRow++) {
+               for (int startCol = 0; startCol <= size - winCondition; startCol++) {
+                    int count = 0;
+                    for (int i = 0; i < winCondition; i++) {
+                         if (board.getGrid()[startRow + i][startCol + i].equals(symbol)) {
+                              count++;
+                              if (count == winCondition) return true;
+                         } else {
+                              break;
+                         }
+                    }
                }
           }
 
-          return mainDiag || antiDiag;
+          for (int startRow = 0; startRow <= size - winCondition; startRow++) {
+               for (int startCol = winCondition - 1; startCol < size; startCol++) {
+                    int count = 0;
+                    for (int i = 0; i < winCondition; i++) {
+                         if (board.getGrid()[startRow + i][startCol - i].equals(symbol)) {
+                              count++;
+                              if (count == winCondition) return true;
+                         } else {
+                              break;
+                         }
+                    }
+               }
+          }
+
+          return false;
      }
 
-     private boolean checkLine(String[] line, String symbol) {
+     private boolean checkLine(String[] line, String symbol, int winCondition) {
+          int count = 0;
           for (String cell : line) {
-               if (!cell.equals(symbol)) {
-                    return false;
+               if (cell.equals(symbol)) {
+                    count++;
+                    if (count == winCondition) return true;
+               } else {
+                    count = 0;
                }
           }
-          return true;
+          return false;
      }
 
      @Override
-     public boolean checkWin(Board board, String symbol){
+     public boolean checkWin(Board board, String symbol) {
           return checkRows(board, symbol) || checkColumns(board, symbol) || checkDiagonals(board, symbol);
      }
 
      @Override
-     public boolean isDraw(Board board){
+     public boolean isDraw(Board board) {
           for (String[] row : board.getGrid()) {
                for (String cell : row) {
                     if (cell.isEmpty()) {
@@ -93,5 +125,4 @@ public class BoardServiceImpl implements BoardService {
           }
           return true;
      }
-
 }
